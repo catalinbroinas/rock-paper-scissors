16af0dc890cb34f09183669eafdb93e79bb08ffc
catalinbroinas <catalinbroinas@yahoo.com>
2023-10-12 19:42:46 +0300

    Update playRound() JS function The function works when click on the buttons from UI

--- style.js
+++ style.js
@@ -1,70 +1,61 @@
-function getComputerChoice()
-{
+const rock = document.querySelector('#rock-button');
+const paper = document.querySelector('#paper-button');
+const scissors = document.querySelector('#scissors-button');
+
+rock.addEventListener('click', () => {
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+});
+paper.addEventListener('click', () => {
+    const playerSelection = 'paper';
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+});
+scissors.addEventListener('click', () => {
+    const playerSelection = 'scissors';
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+});
+
+function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     const computerChoice = ['rock', 'paper', 'scissors'];
     return computerChoice[randomNumber];
 }
-function getPlayerChoice()
-{
-    let playerChoice = prompt("Rock, paper or scissors?");
-    playerChoice = playerChoice.toLowerCase();
-
-    if(playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors")
-    {
-        return playerChoice;
-    }
-    else
-    {
-        return false;
-    }
+function getPlayerChoice(playerChoice) {
+    return playerChoice;
 }
-function playRound(playerSelection, computerSelection)
-{
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    let message = 'ERROR';
 
-    if(playerSelection)
-    {
-        if(playerSelection === computerSelection)
-        {
-            return message = "equality";
+    if (playerSelection === computerSelection) {
+        message = "equality";
+    }
+    else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            message = "computer";
         }
-        else if(playerSelection === "rock")
-        {
-            if(computerSelection === "paper")
-            {
-                return message = "computer";
-            }
-            else
-            {
-                return message = "player";
-            }
-        }
-        else if (playerSelection === "paper")
-        {
-            if(computerSelection === "rock")
-            {
-                return message = "player";
-            }
-            else
-            {
-                return message = "computer";
-            }
-        }
-        else
-        {
-            if(computerSelection === "paper")
-            {
-                return message = "computer";
-            }
-            else
-            {
-                return message = "player";
-            }
+        else {
+            message = "player";
         }
     }
-    else
-    {
-        return message = "Your choice is nor valid!";
+    else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            message = "player";
+        }
+        else {
+            message = "computer";
+        }
     }
+    else {
+        if (computerSelection === "paper") {
+            message = "player";
+        }
+        else {
+            message = "computer";
+        }
+    }
+
+    return message;
 }
