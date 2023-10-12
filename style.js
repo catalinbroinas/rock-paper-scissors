c76cc41fc8747baffdc98a1d6e2e9b4ef350e627
catalinbroinas <catalinbroinas@yahoo.com>
2023-10-12 21:13:04 +0300

    Add 2 JS functions: getResult() and playRound() getResult() return round`s result, playRound() add result in UI

--- style.js
+++ style.js
@@ -2,21 +2,8 @@ const rock = document.querySelector('#rock-button');
 const paper = document.querySelector('#paper-button');
 const scissors = document.querySelector('#scissors-button');
 
-rock.addEventListener('click', () => {
-    const playerSelection = 'rock';
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-});
-paper.addEventListener('click', () => {
-    const playerSelection = 'paper';
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-});
-scissors.addEventListener('click', () => {
-    const playerSelection = 'scissors';
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-});
+const player = document.querySelector('#player-score');
+const computer = document.querySelector('#computer-score');
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -26,7 +13,7 @@ function getComputerChoice() {
 function getPlayerChoice(playerChoice) {
     return playerChoice;
 }
-function playRound(playerSelection, computerSelection) {
+function getResult(playerSelection, computerSelection) {
     let message = 'ERROR';
 
     if (playerSelection === computerSelection) {
@@ -59,3 +46,48 @@ function playRound(playerSelection, computerSelection) {
 
     return message;
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerScore = parseInt(player.textContent);
+    let computerScore = parseInt(computer.textContent);
+    
+    for(let i = 0; i < 1; i++)
+    {
+        let whoWin = getResult(playerSelection, computerSelection);
+        if(whoWin === 'player')
+        {
+            playerScore++;
+            player.textContent = playerScore;
+
+        }
+        else if(whoWin === 'computer')
+        {
+            computerScore++;
+            computer.textContent = computerScore;
+        }
+        else
+        {
+            playerScore += 0;
+            computerScore += 0;
+            player.textContent = playerScore;
+            computer.textContent = computerScore;
+        }
+    }
+    
+}
+
+rock.addEventListener('click', () => {
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+paper.addEventListener('click', () => {
+    const playerSelection = 'paper';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+scissors.addEventListener('click', () => {
+    const playerSelection = 'scissors';
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
